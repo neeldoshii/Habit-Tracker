c6026c6be51c790b7f60ccca2cb714dbdb3bf539
Neel Doshi <neeldoshi147@gmail.com>
2024-06-22 22:27:36 +0530

    Revert : Google Sign In Button Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,6 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.play.services.base)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

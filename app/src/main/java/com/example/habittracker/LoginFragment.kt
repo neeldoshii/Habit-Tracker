8bf9ef20a695f448923cd0c3999ebe111084ca0e
Neel Doshi <neeldoshi147@gmail.com>
2024-06-28 00:27:48 +0530

    Reformated the code into functions

--- app/src/main/java/com/example/habittracker/LoginFragment.kt
+++ app/src/main/java/com/example/habittracker/LoginFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
 import androidx.fragment.app.Fragment
@@ -31,89 +30,98 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
-
-        val rawNonce = UUID.randomUUID().toString()
-        val bytes = rawNonce.toByteArray()
-        val md = MessageDigest.getInstance("SHA-256")
-        val digest = md.digest(bytes)
-        val hashedNonce = digest.fold("") { str, it -> str + "%02x".format(it) }
-
         // TODO(Dependency INJECT)
         firebaseAuth = FirebaseAuth.getInstance()
 
-        val credentialManager = CredentialManager.create(requireActivity())
-
         binding.btnGoogle.setOnClickListener {
-            // Docs Ref : https://developer.android.com/identity/sign-in/credential-manager-siwg
-            val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
-                .setFilterByAuthorizedAccounts(false)
-                .setServerClientId(BuildConfig.WEB_CLIENT_ID)
-                .setNonce(hashedNonce)
-                .build()
-
-            // Ref : for Firebase Google Auth Integration
-            // https://firebase.google.com/docs/auth/android/google-signin
-            // TODO Call this in a switch case func to handle (email, anonymous login, google auth)
-            // TODO Extract it into VIEWMODEL MVVM Logic
-            try {
-                val request : GetCredentialRequest = GetCredentialRequest.Builder()
-                    .addCredentialOption(googleIdOption)
-                    .build()
-
-                viewLifecycleOwner.lifecycleScope.launch {
-                    val result = credentialManager.getCredential(
-                        request = request,
-                        context = requireActivity()
-                    )
-                    val credentials = result.credential
-
-                    val googleIdToken = GoogleIdTokenCredential
-                        .createFrom(credentials.data)
-
-                    val firebaseCredential = GoogleAuthProvider.getCredential(googleIdToken.idToken, null)
-                    firebaseAuth.signInWithCredential(firebaseCredential)
-                        .addOnCompleteListener(requireActivity()){ task->
-                            if (task.isSuccessful){
-                                val user = firebaseAuth.currentUser
-                                //TODO update UI
-                            }
-                            else{
-                                Timber.w(task.exception, "signInWithCredential:failure")
-                                //TODO update UI
-                            }
-                        }
-                }
-
-            }catch (e : Exception){
-                //TODO
-                Timber.tag("Exception").e(e.toString())
-            }
+            userAuthentication(googleAuthentication = true)
         }
 
         binding.guestButton.setOnClickListener {
-            // TODO Extract in function for cleaner code
-            // Ref : https://firebase.google.com/docs/auth/android/anonymous-auth
-            firebaseAuth.signInAnonymously()
-                .addOnCompleteListener(requireActivity()) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Timber.d("signInAnonymously:success")
-                        val user = firebaseAuth.currentUser
-                        //updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Timber.w(task.exception, "signInAnonymously:failure")
-                        Toast.makeText(
-                            context,
-                            "Authentication failed.",
-                            Toast.LENGTH_SHORT,
-                        ).show()
-                        //updateUI(null)
-                    }
-
-                }
+            userAuthentication(anonymousLogin = true)
         }
         return binding.root
+    }
+
+    private fun userAuthentication(
+        googleAuthentication: Boolean = false,
+        anonymousLogin: Boolean = false,
+        emailLogin: Boolean = false
+    ) {
+        when {
+            // Ref : for Firebase Google Auth Integration
+            // https://firebase.google.com/docs/auth/android/google-signin
+            // TODO Extract all it into VIEWMODEL MVVM Logic
+            googleAuthentication -> {
+                val rawNonce = UUID.randomUUID().toString()
+                val bytes = rawNonce.toByteArray()
+                val md = MessageDigest.getInstance("SHA-256")
+                val digest = md.digest(bytes)
+                val hashedNonce = digest.fold("") { str, it -> str + "%02x".format(it) }
+                val credentialManager = CredentialManager.create(requireActivity())
+                // Docs Ref : https://developer.android.com/identity/sign-in/credential-manager-siwg
+                val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId(BuildConfig.WEB_CLIENT_ID)
+                    .setNonce(hashedNonce)
+                    .build()
+
+                try {
+                    val request : GetCredentialRequest = GetCredentialRequest.Builder()
+                        .addCredentialOption(googleIdOption)
+                        .build()
+
+                    lifecycleScope.launch {
+                        val result = credentialManager.getCredential(
+                            request = request,
+                            context = requireActivity()
+                        )
+                        val credentials = result.credential
+
+                        val googleIdToken = GoogleIdTokenCredential
+                            .createFrom(credentials.data)
+
+                        val firebaseCredential = GoogleAuthProvider.getCredential(googleIdToken.idToken, null)
+                        firebaseAuth.signInWithCredential(firebaseCredential)
+                            .addOnCompleteListener(requireActivity()){ task->
+                                if (task.isSuccessful){
+                                    val user = firebaseAuth.currentUser
+                                    //TODO update UI
+                                }
+                                else{
+                                    Timber.w(task.exception, "signInWithCredential:failure")
+                                    //TODO update UI
+                                }
+                            }
+                    }
+
+                }catch (e : Exception){
+                    //TODO
+                    Timber.tag("Exception").e(e.toString())
+                }
+            }
+
+            anonymousLogin -> {
+                // Ref : https://firebase.google.com/docs/auth/android/anonymous-auth
+                lifecycleScope.launch {
+                    firebaseAuth.signInAnonymously()
+                        .addOnCompleteListener(requireActivity()) { task ->
+                            if (task.isSuccessful) {
+                                Timber.d("signInAnonymously:success")
+                                //val user = firebaseAuth.currentUser
+                                //updateUI(user)
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Timber.w(task.exception, "signInAnonymously:failure")
+                                //updateUI(null)
+                            }
+                        }
+                }
+            }
+            emailLogin -> {
+                // TODO Not Yet Implemented
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
